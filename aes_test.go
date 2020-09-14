9b47e2c8a2554eb69ddc1a0f22db1dc08ca48a2c
Hank Shen <swhbox@foxmail.com>
2020-09-14 18:56:29 +0800

    update

---
+++ aes_test.go
@@ -0,0 +1,31 @@
+package codec
+
+import (
+	"fmt"
+	"strings"
+	"testing"
+
+	"github.com/webx-top/com"
+)
+
+func TestAes(t *testing.T) {
+	var (
+		paykey  = "ziR0QKsTUfMOuochC9RfCdmfHECorQAP"
+		reqInfo = `YYwp8C48th0wnQzTqeI+41pflB26v+smFj9z6h9RPBgxTyZyxc+4YNEz7QEgZNWj/6rIb2MfyWMZmCc41CfjKSssoSZPXxOhUayb6KvNSZ1p6frOX1PDWzhyruXK7ouNND+gDsG4yZ0XXzsL4/pYNwLLba/71QrnkJ/BHcByk4EXnglju5DLup9pJQSnTxjomI9Rxu57m9jg5lLQFxMWXyeASZJNvof0ulnHlWJswS4OxKOkmW7VEyKyLGV6npoOm03Qsx2wkRxLsSa9gPpg4hdaReeUqh1FMbm7aWjyrVYT/MEZWg98p4GomEIYvz34XfDncTezX4bf/ZiSLXt79aE1/YTZrYfymXeCrGjlbe0rg/T2ezJHAC870u2vsVbY1/KcE2A443N+DEnAziXlBQ1AeWq3Rqk/O6/TMM0lomzgctAOiAMg+bh5+Gu1ubA9O3E+vehULydD5qx2o6i3+qA9ORbH415NyRrQdeFq5vmCiRikp5xYptWiGZA0tkoaLKMPQ4ndE5gWHqiBbGPfULZWokI+QjjhhBmwgbd6J0VqpRorwOuzC/BHdkP72DCdNcm7IDUpggnzBIy0+seWIkcHEryKjge3YDHpJeQCqrAH0CgxXHDt1xtbQbST1VqFyuhPhUjDXMXrknrGPN/oE1t0rLRq+78cI+k8xe5E6seeUXQsEe8r3358mpcDYSmXWSXVZxK6er9EF98APqHwcndyEJD2YyCh/mMVhERuX+7kjlRXSiNUWa/Cv/XAKFQuvUYA5ea2eYWtPRHa4DpyuF1SNsaqVKfgqKXZrJHfAgslVpSVqUpX4zkKszHF4kwMZO3M7J1P94Mxa7Tm9mTOJePOoHPXeEB+m9rX6pSfoi3mJDQ5inJ+Vc4gOkg/Wd/lqiy6TTyP/dHDN6/v+AuJx5AXBo/2NDD3fWhHjkqEKIuARr2ClZt9ZRQO4HkXdZo7CN06sGCHk48Tg8PmxnxKcMZm7Aoquv5yMIM2gWSWIRJhwJ8cUpafIHc+GesDlbF6Zbt+/KXkafJAQq2RklEN+WvZ/zFz113EPgWPjp16TwBoziq96MMekvWKY/vdhjol8VFtGH9F61Oy1Xwf6DJtPw==`
+	)
+
+	crypto := NewAesCrypto(`AES-256-ECB`)
+	plaintext := `admpub.com`
+	crypted := crypto.Encode(plaintext, paykey)
+	if crypto.Decode(crypted, paykey) != plaintext {
+		t.Fatal(`failed`)
+	}
+	t.Fatal(crypto.Decode(crypted, paykey))
+	text, err := com.Base64Decode(reqInfo)
+	if err != nil {
+		t.Fatal(text, err)
+	}
+	plaintext = crypto.Decode(text, strings.ToLower(com.Md5(paykey)))
+	fmt.Println(string(plaintext))
+	panic(plaintext)
+}
