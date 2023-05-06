9326c086d1d92306610a1537865d9f43ab16d327
Hank Shen <swh@admpub.com>
2023-05-06 22:50:04 +0800

    update

--- aes_test.go
+++ aes_test.go
@@ -1,12 +1,13 @@
 package codec
 
 import (
+	"encoding/hex"
 	"fmt"
 	"testing"
 	"time"
 )
 
-func TestAes(t *testing.T) {
+func TestAES(t *testing.T) {
 	var (
 		paykey = fmt.Sprintf(`%032d`, time.Now().UnixMicro())
 	)
@@ -18,30 +19,48 @@ func TestAes(t *testing.T) {
 		t.Fatal(`failed`)
 	}
 	//t.Fatal(crypto.Decode(crypted, paykey))
+}
 
-	GenAESKey([]byte(`2`))
+func TestFixedKeyDefault(t *testing.T) {
+	pwd := `123`
+	key := Md5bytes([]byte(pwd))
+	key2 := Md5str(pwd)
+	if string(key) != key2 {
+		t.Fatalf(`%s != %s`, key, key2)
+	}
+	t.Logf(`md5key: %s`, key)
+	fixedKey := FixedKeyDefault(16, key)
+	t.Logf(`fixedKey: %s`, hex.EncodeToString(fixedKey))
+
+	pwd = `1234`
+	key = Md5bytes([]byte(pwd))
+	t.Logf(`md5key: %s`, key)
+	fixedKey = FixedKeyDefault(16, key)
+	t.Logf(`fixedKey: %s`, hex.EncodeToString(fixedKey))
 }
 
 func TestGenAESKey(t *testing.T) {
-	key := GenAESKey([]byte(`2`))
+	aesKey := newAESKey(KeyAES128)
+	aesKey.SetKeyFixer(FixedKeyByWhitespacePrefix)
+	key := aesKey.GenKey([]byte(`2`))
 	t.Logf(`key: [%s]`, key)
 	if string(key) != `               2` {
 		t.Fatal(`failed`)
 	}
 
-	key = GenAESKey([]byte(`123`))
+	key = aesKey.GenKey([]byte(`123`))
 	t.Logf(`key: [%s]`, key)
 	if string(key) != `             123` {
 		t.Fatal(`failed`)
 	}
 
-	key = GenAESKey([]byte(``))
+	key = aesKey.GenKey([]byte(``))
 	t.Logf(`key: [%s]`, key)
 	if string(key) != `                ` {
 		t.Fatal(`failed`)
 	}
 
-	key = GenAESKey([]byte(`12345678901234567890`))
+	key = aesKey.GenKey([]byte(`12345678901234567890`))
 	t.Logf(`key: [%s]`, key)
 	if string(key) != `1234567890123456` {
 		t.Fatal(`failed`)
